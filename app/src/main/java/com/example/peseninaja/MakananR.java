d15eb6c61e40503b080b6938eb70ab1fc565bb20
ASUS <kevinprmn25@gmail.com>
2022-06-01 15:34:49 +0800

    Progress

--- app/src/main/java/com/example/peseninaja/MakananR.java
+++ app/src/main/java/com/example/peseninaja/MakananR.java
@@ -1,33 +1,14 @@
 package com.example.peseninaja;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-import java.util.ArrayList;
-
 public class MakananR extends AppCompatActivity {
-
-    private RecyclerView rvmakanan;
-    private ArrayList<Makanan> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_makanan_r);
-
-        rvmakanan = findViewById(R.id.rvMakanan);
-        rvmakanan.setHasFixedSize(true);
-
-        list.addAll(MakananData.getListData());
-        showRecyclerList();
-    }
-
-    private void showRecyclerList(){
-        rvmakanan.setLayoutManager(new LinearLayoutManager(this));
-        ListMakananAdapter listMakananAdapter = new ListMakananAdapter(list);
-        rvmakanan.setAdapter(listMakananAdapter);
     }
 }
